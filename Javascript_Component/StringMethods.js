6ad9f021958c7044dbe97bd5cf37f61bec290829
Blacklion567 <naviedaj567@gmail.com>
2023-11-30 20:35:17 +0800

    Javascript_Component

---
+++ Javascript_Component/StringMethods.js
@@ -0,0 +1,15 @@
+//String Declarations
+const name = "Blacklion567";
+const age = 20;
+const job = 'Programmer';
+
+const person = `My name is ${name}, I'm ${age} years old and my job is ${job}`;
+
+console.log(person);
+
+
+//String functions
+console.log(person.indexOf(job));           // 56
+console.log(person.lastIndexOf(name));      // 11
+console.log(job.charAt(2));                 // o
+console.log(job[3]);                        // g
